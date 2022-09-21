0097ab8f3a276f515a51aa6b40ad2599f48144fb
Viktor Persson <viktor.persson@arcsin.se>
2022-09-21 12:28:30 +0200

    Update tests for server address autocompletion.

--- internal/pkg/carmack/autocomplete/serveraddress_test.go
+++ internal/pkg/carmack/autocomplete/serveraddress_test.go
@@ -8,23 +8,38 @@ import (
 	"github.com/vikpe/carmack/internal/pkg/carmack/autocomplete"
 )
 
-func TestServerAddress(t *testing.T) {
-	dataOption := &discordgo.ApplicationCommandInteractionDataOption{
+func getDataOption(value string) *discordgo.ApplicationCommandInteractionDataOption {
+	return &discordgo.ApplicationCommandInteractionDataOption{
 		Name:    "address",
 		Type:    discordgo.ApplicationCommandOptionString,
-		Value:   "foppa",
+		Value:   value,
 		Focused: true,
 	}
+}
 
-	choices := autocomplete.ServerAddress(dataOption)
-	expect := [][]string{
-		{"foppa", "choice_custom"},
-		{"qw.foppa.dk:27500", "qw.foppa.dk:27500"},
-		{"qw.foppa.dk:27501", "qw.foppa.dk:27501"},
-		{"qw.foppa.dk:27502", "qw.foppa.dk:27502"},
-		{"qw.foppa.dk:27503", "qw.foppa.dk:27503"},
-		{"qw.foppa.dk:27504", "qw.foppa.dk:27504"},
-		{"qw.foppa.dk:27505", "qw.foppa.dk:27505"},
-	}
-	assert.Equal(t, expect, choices)
+func TestServerAddress(t *testing.T) {
+	t.Run("single needle", func(t *testing.T) {
+		dataOption := getDataOption("foppa")
+		choices := autocomplete.ServerAddress(dataOption)
+		expect := [][]string{
+			{"foppa", "choice_custom"},
+			{"qw.foppa.dk:27500", "qw.foppa.dk:27500"},
+			{"qw.foppa.dk:27501", "qw.foppa.dk:27501"},
+			{"qw.foppa.dk:27502", "qw.foppa.dk:27502"},
+			{"qw.foppa.dk:27503", "qw.foppa.dk:27503"},
+			{"qw.foppa.dk:27504", "qw.foppa.dk:27504"},
+			{"qw.foppa.dk:27505", "qw.foppa.dk:27505"},
+		}
+		assert.Equal(t, expect, choices)
+	})
+
+	t.Run("multiple needles", func(t *testing.T) {
+		dataOption := getDataOption("foppa 502")
+		choices := autocomplete.ServerAddress(dataOption)
+		expect := [][]string{
+			{"foppa 502", "choice_custom"},
+			{"qw.foppa.dk:27502", "qw.foppa.dk:27502"},
+		}
+		assert.Equal(t, expect, choices)
+	})
 }
