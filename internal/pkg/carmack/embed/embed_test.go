81693a0881ce96370bdb0985dca61f7a226bc634
Viktor Persson <viktor.persson@arcsin.se>
2022-09-21 15:18:18 +0200

    Add test for server embed.

---
+++ internal/pkg/carmack/embed/embed_test.go
@@ -0,0 +1,41 @@
+package embed_test
+
+import (
+	"testing"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/go-playground/assert/v2"
+	"github.com/vikpe/carmack/internal/pkg/carmack/embed"
+	"github.com/vikpe/carmack/internal/pkg/hub"
+)
+
+func TestFromStream(t *testing.T) {
+	stream := hub.Stream{
+		Channel:       "QuakeWorld",
+		Url:           "https://twitch.tv/Quakeworld",
+		Title:         "1on1: dough vs grl [ztndm3]",
+		ViewerCount:   5,
+		ServerAddress: "qw.foppa.dk:27502",
+	}
+
+	expect := discordgo.MessageEmbed{
+		Title:       stream.Channel,
+		URL:         stream.Url,
+		Description: stream.Title,
+		Thumbnail: &discordgo.MessageEmbedThumbnail{
+			URL: "https://static-cdn.jtvnw.net/previews-ttv/live_user_quakeworld-428x240.jpg",
+		},
+		Fields: []*discordgo.MessageEmbedField{
+			{
+				Name:   "Server",
+				Value:  "`qw.foppa.dk:27502`",
+				Inline: true,
+			},
+		},
+		Footer: &discordgo.MessageEmbedFooter{
+			Text: "5 viewers",
+		},
+	}
+
+	assert.Equal(t, expect, embed.FromStream(stream))
+}
