a3e6d862bcc58bafeb293becf621a1eb0698b880
Viktor Persson <viktor.persson@arcsin.se>
2022-10-03 19:12:55 +0200

    Add test for mvdsv server embed.

--- internal/pkg/carmack/embed/embed_test.go
+++ internal/pkg/carmack/embed/embed_test.go
@@ -7,7 +7,93 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/vikpe/carmack/internal/pkg/carmack/embed"
 	"github.com/vikpe/qw-hub-api/types"
+	"github.com/vikpe/serverstat/qserver/geo"
+	"github.com/vikpe/serverstat/qserver/mvdsv"
+	"github.com/vikpe/serverstat/qserver/mvdsv/qmode"
+	"github.com/vikpe/serverstat/qserver/mvdsv/qstatus"
+	"github.com/vikpe/serverstat/qserver/mvdsv/qtvstream"
+	"github.com/vikpe/serverstat/qserver/qclient"
+	"github.com/vikpe/serverstat/qserver/qclient/slots"
+	"github.com/vikpe/serverstat/qserver/qsettings"
+	"github.com/vikpe/serverstat/qserver/qtime"
+	"github.com/vikpe/serverstat/qtext/qstring"
 )
+
+func TestFromMvdsvServer(t *testing.T) {
+	server := mvdsv.Mvdsv{
+		Address: "quake.se:28501",
+		Mode:    qmode.Mode("4on4"),
+		Title:   "4on4: red (XantoM) [dm2]",
+		Status: qstatus.Status{
+			Name:        "Started",
+			Description: "3 min left",
+		},
+		Time: qtime.Time{
+			Elapsed:   7,
+			Total:     10,
+			Remaining: 3,
+		},
+		Players: []qclient.Client{{
+			Name:   qstring.New("XantoM"),
+			Team:   qstring.New("red"),
+			Skin:   "",
+			Colors: [2]uint8{13, 13},
+			Frags:  2,
+			Ping:   38,
+			Time:   4,
+		},
+		},
+		PlayerSlots: slots.Slots{
+			Used:  1,
+			Total: 8,
+			Free:  7,
+		},
+		SpectatorNames: []string{"[ServeMe]"},
+		SpectatorSlots: slots.Slots{
+			Used:  1,
+			Total: 4,
+			Free:  3,
+		},
+		Settings: qsettings.Settings{"map": "dm2", "*gamedir": "qw", "status": "3 min left", "timelimit": "10", "maxclients": "8", "maxspectators": "4", "teamplay": "2"},
+		QtvStream: qtvstream.QtvStream{
+			Title:          "QUAKE.SE KTX Qtv (1)",
+			Url:            "1@quake.se:28000",
+			ID:             1,
+			Address:        "quake.se:28000",
+			SpectatorNames: []string{},
+			SpectatorCount: 0,
+		},
+		Geo: geo.Location{
+			CC:          "SE",
+			Country:     "Sweden",
+			Region:      "Europe",
+			City:        "Hagersten",
+			Coordinates: [2]float32{59.2885, 17.9612},
+		},
+		Score: 5,
+	}
+
+	expect := &discordgo.MessageEmbed{
+		Title:       ":flag_se: quake.se:28501",
+		Description: "4on4 on dm2\n",
+		Color:       0x0c2aac,
+		Fields: []*discordgo.MessageEmbedField{
+			{
+				Name:  "Players (1/8)",
+				Value: "XantoM",
+			},
+			{
+				Name:  "Spectators (1/4)",
+				Value: "[ServeMe]",
+			},
+		},
+		Footer: &discordgo.MessageEmbedFooter{
+			Text: "Started - 3 min left",
+		},
+	}
+
+	assert.Equal(t, expect, embed.FromMvdsvServer(server))
+}
 
 func TestFromStream(t *testing.T) {
 	t.Run("base test", func(t *testing.T) {
